f6832494994dfec39f8b29e73ad079ecaa0b0dcf
zemyman <130682806+zemyman@users.noreply.github.com>
2023-06-15 13:27:45 -0400

    fixed borders

--- PokemonGame-/Player.java
+++ PokemonGame-/Player.java
@@ -20,7 +20,69 @@ public class Player extends Actor
          GreenfootImage east = new GreenfootImage("PlayerE.png");
          GreenfootImage south = new GreenfootImage("PlayerS.png");
        //Movement
-      
+      if(isTouching(smallVBound.class)){
+            if(Greenfoot.isKeyDown("a")){
+                setLocation(getX()  + speed , getY());
+            }else{
+                setLocation(getX()  - speed , getY() );
+            }
+        }
+        if(isTouching(SmallHbounds.class)){
+           if(Greenfoot.isKeyDown("w")){
+                setLocation(getX()  , getY() + speed);
+            }else{
+                setLocation(getX()   , getY() - speed);
+            }
+        }
+        if(isTouching(Bounds.class)){
+            if(Greenfoot.isKeyDown("w")){
+                setLocation(getX()  , getY() + speed);
+            }else{
+                setLocation(getX()  , getY() - speed);
+            }
+        }
+        if(isTouching(BoundsSide.class)){
+            if(Greenfoot.isKeyDown("a")){
+                setLocation(getX()  + speed , getY());
+            }else{
+                setLocation(getX()  - speed , getY() );
+            }
+            
+        }
+       
+        if(Greenfoot.isKeyDown("w") || Greenfoot.isKeyDown("s")){
+          
+          if (Greenfoot.isKeyDown("w")){
+            deltaY = speed * -1;
+            setImage(north);
+            }
+            else if (Greenfoot.isKeyDown("s")){
+            deltaY = speed;
+            setImage(south);
+            }
+                   }
+         else{
+            deltaY = 0;
+             }
+             
+        if(Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("d")) {
+           
+           if (Greenfoot.isKeyDown("a")){
+            deltaX = speed * -1;
+            setImage(west);
+            }
+            else if (Greenfoot.isKeyDown("d")){
+            deltaX = speed;
+            setImage(east);
+            }
+       
+        
+        }else{
+            deltaX = 0;
+            }
+        //updating the players posistion
+        
+        setLocation(getX() +  deltaX , getY() + deltaY);
     
 }
     public static void whatS(int start){
