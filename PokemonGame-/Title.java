d61d2210e7c65b407ac5774fb5e61e57c0193c5b
Jam1sonnn <GordonJ@limestone.on.ca>
2023-06-15 14:28:44 -0400

    Title screen
    Added animation and attached it to pokemon select

--- PokemonGame-/Title.java
+++ PokemonGame-/Title.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Title extends World
 {
-
+    public static int x= 0;
     /**
      * Constructor for objects of class Title.
      * 
@@ -17,41 +17,8 @@ public class Title extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
-        Start w = new Start();
+        addObject(new titlescreen(), 300, 200);
         
-        if(Greenfoot.isKeyDown("Enter") ){
-            Greenfoot.setWorld(w);
-        }
-        
-         GreenfootImage t1 = new GreenfootImage("T1.png");
-         GreenfootImage t2 = new GreenfootImage("T2.png");
-         GreenfootImage t3 = new GreenfootImage("T3.png");
-         GreenfootImage t4 = new GreenfootImage("T4.png");
-         GreenfootImage t5 = new GreenfootImage("T5.png");
-         GreenfootImage t6 = new GreenfootImage("T6.png");
-         GreenfootImage t7 = new GreenfootImage("T7.png");
-         GreenfootImage t8 = new GreenfootImage("T8.png");
-         GreenfootImage t9 = new GreenfootImage("T9.png");
-         GreenfootImage t10 = new GreenfootImage("T10.png");
-         GreenfootImage t11 = new GreenfootImage("T11.png");
-         GreenfootImage t12 = new GreenfootImage("T12.png");
-         GreenfootImage t13 = new GreenfootImage("T13.png");
-         GreenfootImage t14 = new GreenfootImage("T14.png");
-         GreenfootImage t15 = new GreenfootImage("T15.png");
-         GreenfootImage t16 = new GreenfootImage("T16.png");
-         GreenfootImage t17 = new GreenfootImage("T17.png");
-         GreenfootImage t18 = new GreenfootImage("T18.png");
-         GreenfootImage t19 = new GreenfootImage("T19.png");
-         GreenfootImage t20 = new GreenfootImage("T20.png");
-         GreenfootImage t21 = new GreenfootImage("T21.png");
-         GreenfootImage t22 = new GreenfootImage("T22.png");
-         GreenfootImage t23 = new GreenfootImage("T23.png");
-         GreenfootImage t24 = new GreenfootImage("T24.png");
-         GreenfootImage t25 = new GreenfootImage("T25.png");
-         GreenfootImage[] images = {t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21, t22, t23, t24, t25};
-         do{
-            
-         }while();
          
     }
 }
